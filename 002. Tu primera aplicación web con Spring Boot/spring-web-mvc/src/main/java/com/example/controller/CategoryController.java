f54dfb3b112f769449d10e89864f7929a07a9f99
DanielG <danielgallegom10@gmail.com>
2025-02-11 12:44:23 +0100

    Update CategoryController.java

--- 002. Tu primera aplicación web con Spring Boot/spring-web-mvc/src/main/java/com/example/controller/CategoryController.java
+++ 002. Tu primera aplicación web con Spring Boot/spring-web-mvc/src/main/java/com/example/controller/CategoryController.java
@@ -1,9 +1,14 @@
 package com.example.controller;
 
+import com.example.entity.Category;
+import com.example.entity.Product;
 import com.example.repository.CategoryRepostory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/categories")
@@ -16,7 +21,19 @@ public class CategoryController {
     }
 
     @GetMapping
-    public 
+    public String findAll(Model model){
+        List<Category> categories = this.repostory.findAll();
+        model.addAttribute("categories", categories);
+        return "category-list";
+    }
+
+    @GetMapping("/new")
+    public String getForm(Model model){
+        model.addAttribute("categories", new Category());
+        return "category-form";
+    }
+
+    
 
 
 
