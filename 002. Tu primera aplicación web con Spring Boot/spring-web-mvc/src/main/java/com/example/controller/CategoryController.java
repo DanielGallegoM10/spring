1557727fd811d97f8d7369b65fcbf1f78279f133
DanielG <danielgallegom10@gmail.com>
2025-02-11 17:02:39 +0100

    metodos category

--- 002. Tu primera aplicación web con Spring Boot/spring-web-mvc/src/main/java/com/example/controller/CategoryController.java
+++ 002. Tu primera aplicación web con Spring Boot/spring-web-mvc/src/main/java/com/example/controller/CategoryController.java
@@ -6,9 +6,13 @@ import com.example.repository.CategoryRepostory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/categories")
@@ -33,8 +37,56 @@ public class CategoryController {
         return "category-form";
     }
 
-    
+    @GetMapping("/{id}/view")
+    public String getCategory(@PathVariable Long id, Model model, RedirectAttributes redirectAttributes){
+        Optional<Category> category = repostory.findById(id);
 
+        if (category.isPresent()) {
+            model.addAttribute("category", category.get());
+            return "category-view"; // Nombre del archivo HTML en templates
+        } else {
+            redirectAttributes.addFlashAttribute("message", "Categoria no encontrada");
+            redirectAttributes.addFlashAttribute("alert", "warning");
+            return "redirect:/categories";
+        }
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editCategory(@PathVariable Long id, Model model, RedirectAttributes redirectAttributes){
+        Optional<Category> category = repostory.findById(id);
+
+        if (category.isPresent()) {
+            model.addAttribute("category", category.get());
+            return "category-edit"; // Nombre del archivo HTML en templates
+        } else {
+            redirectAttributes.addFlashAttribute("message", "Categoria no encontrada");
+            redirectAttributes.addFlashAttribute("alert", "warning");
+            return "redirect:/categories";
+        }
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteCategory(@PathVariable Long id, Model model, RedirectAttributes redirectAttributes){
+        Optional<Category> category = repostory.findById(id);
+
+        if (category.isPresent()) {
+            model.addAttribute("category", category.get());
+            return "category-delete"; // Nombre del archivo HTML en templates
+        } else {
+            redirectAttributes.addFlashAttribute("message", "Categoria no encontrada");
+            redirectAttributes.addFlashAttribute("alert", "warning");
+            return "redirect:/categories";
+        }
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam("keyword") String keyword, Model model){
+
+        List<Category> categories = this.repostory.findCategoriesByDescription(keyword);
+        model.addAttribute("categories", categories);
+        return "category-search";
+    }
+    
 
 
 }
